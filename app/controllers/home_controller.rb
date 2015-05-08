d110f7d45e40489200d2825e57b862d2ea745673
Rory G <rory.ginty@aviva.ie>
2015-05-08 13:10:32 +0100

    Latest Sprint
    Sprint

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+
+	def index
+	
+		if Circle.find(current_user.circle_id).group_code == "Aviva"
+			redirect_to rewards_path
+		elsif Circle.find(current_user.circle_id).group_code == "Empty"
+			redirect_to circles_join_path
+		elsif Policy.where("user_id = ?", current_user.id).count == 0
+			redirect_to policies_setup_path
+		end
+		
+	end
+
+end
